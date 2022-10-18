ae3862c7e73f331b0c6c577c3bd9316e6e8d8047
adityabhartib <adityabharti.ab@gmail.com>
2022-10-18 18:46:07 +0530

    Added :-UC1 Ability to create contacts

---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Contacts {
+    private String firstname;
+    private String lastname;
+    private String city;
+    private String state;
+    private Double Pin;
+    private Double phonenumber;
+    private String email;
+
+
+}

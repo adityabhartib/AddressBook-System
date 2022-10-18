23bfc397c2d721a034c07c186e0a357235365bef
adityabhartib <adityabharti.ab@gmail.com>
2022-10-18 19:35:43 +0530

    Added :- UC2 Ability to add contacts

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -6,9 +6,71 @@ public class Contacts {
     private String Address;
     private String city;
     private String state;
-    private Double Pin;
-    private Double phonenumber;
+    private long Pin;
+    private Long phonenumber;
     private String email;
 
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getPin() {
+        return Pin;
+    }
+
+    public void setPin(long pin) {
+        Pin = pin;
+    }
+
+    public long getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(long phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
